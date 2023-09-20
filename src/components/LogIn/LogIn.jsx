82ab9537a49ecb4f63820fd902f04b18cb614de0
Jasvaran Mann <mannjesse355@gmail.com>
2023-09-19 23:10:45 -0700

    Added handleSubmit for Logging In

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -2,11 +2,16 @@
 import './LogIn.css'
 import { Link } from 'react-router-dom'
 import Nav from '../Nav/Nav'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 function LogIn() {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+
+
+    useEffect(() => {
+        console.log(username)
+    })
 
     function handleChange(e){
         if (e.target.id === 'username'){
@@ -17,19 +22,43 @@ function LogIn() {
         }
     }
 
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:3000/user/login', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+            .then((response) => {
+                console.log('response before .json()------>' , response)
+                return response.json()
+            })
+            .then((response) => {
+                console.log(response)
+            })
+
+    }
+
     return (
         <>
             <Nav />
             <h1 className='text-center mt-5'>Log In</h1>
             <div className="container-fluid d-flex flex-column justify-content-start align-items-center" style={{height: "100vh"}}>
-                <form action="" className='d-flex flex-column justify-content-center' style={{height: "500px"}}>
+                <form action="" className='d-flex flex-column justify-content-center' style={{height: "500px"}} onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="username" className="form-label">Username</label>
                         <input type="text" name="username" id='username' className="form-control" onChange={handleChange} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" name="password" id='password' className="form-control" />
+                        <input type="password" name="password" id='password' className="form-control" onChange={handleChange} />
                     </div>
                     <button type="submit" className="btn btn-primary btn-lg mb-3">Submit</button>
                     <div className="mb-3">
