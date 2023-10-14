141c5d06f255f8302aabb976cea48adb1e58d2c4
Jasvaran Mann <mannjesse355@gmail.com>
2023-10-13 17:26:34 -0700

    Added Comment Form Component to page

--- src/components/BlogPost/BlogPost.jsx
+++ src/components/BlogPost/BlogPost.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import './BlogPost.css'
+import CommentForm from '../CommentForm/CommentForm'
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Nav from "../Nav/Nav"
@@ -29,23 +30,32 @@ function BlogPost(props){
 
     }
 
-    // TODO: just added img; Finish layout
 
  
     return (
-        <>
-            <Nav />
-            <div className="container-fluid d-flex flex-column justify-content-center align-items-center " id="main-container">
+        
+        <div className="container-fluid d-flex flex-column justify-content-center align-items-center " id="main-container">
+
+            <div className="container-fluid" id='header-container'>
+                <Nav />
+            </div>
+
+            <div className="container-fluid d-flex flex-column justify-content-center align-items-center" id="body-container">
                 <h1>{post.title}</h1>
                 {post.blogpic ? <img src={"data:" + post.blogpic.contentType + 
                     ';base64,' + btoa(String.fromCharCode.apply(null, new Uint8Array(post.blogpic.data.data)))} alt=""></img>
-                              : ""}
+                                : ""}
 
                 <div className="container-fluid" id="blog-text-container">
                     {post.text}
                 </div>
+
             </div>
-        </>
+
+            <CommentForm post={post} />
+
+        </div>
+
     )
 }
 
