9dd72ad7779f2d8e0fe6a95a2c8ce13128c61e0c
Jasvaran Mann <mannjesse355@gmail.com>
2023-10-13 17:28:56 -0700

    Comment Form component initial commit

---
+++ src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-vars */
+import './CommentForm.css'
+import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react';
+function CommentForm(props) {
+
+    const [comment, setComment] = useState("");
+
+
+    useEffect(() => {
+        
+    })
+
+    function handleChange(e){
+        e.preventDefault()
+        if (e.target.id === 'comment-text'){
+            setComment(e.target.value)
+        }
+    }
+
+    async function createComment(e){
+
+        const response = await fetch("http://localhost:3000/comments", {
+            method: 'POST',
+            credentials: 'include',
+            mode: 'cors',
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                text: comment,
+                postId: props.post._id
+            })
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data)
+            })
+        
+    }
+
+    return (
+
+        <div className="container-fluid" id='main-container'>
+            <form action="" onSubmit={createComment}>
+                <div className="d-flex flex-column mb-3">
+                    <label htmlFor="comment-text" className="form-label">Write your Comment In The Box Below</label>
+                    <input type="text" className='form-control' name="comment-text" id="comment-text" onChange={handleChange} required />
+                </div>
+                <div className="mb-3">
+                    <button type="submit" className="btn btn-primary btn-sm">Add Comment</button>
+                </div>
+            </form>
+        </div>
+
+    )
+}
+
+CommentForm.propTypes = {
+    post: PropTypes.object
+}
+
+
+export default CommentForm
